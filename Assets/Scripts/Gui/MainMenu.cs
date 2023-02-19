73b4485c7f79611a12ef877b0bb64a3e616f3ca0
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2023-02-19 13:41:15 -0300

    Add random avatars to game manu

--- Assets/Scripts/Gui/MainMenu.cs
+++ Assets/Scripts/Gui/MainMenu.cs
@@ -6,9 +6,20 @@ public class MainMenu : MonoBehaviour
 {
     public AudioClip MusicClip;
 
+    public BodyPartsController BodyPartsController;
+
+    public GuiAvatar[] Avatars;
+
+    public float AvatarChangeInterval = 1f;
+
+    private float _avatarChangeCountdown = 0f;
+
     public void Start()
     {
         EazySoundManager.PlayMusic(this.MusicClip, 1.0f, true, false);
+
+        this._avatarChangeCountdown = this.AvatarChangeInterval;
+        this.ChangeAppearances();
     }
 
     public void OnStartButtonClicked()
@@ -24,5 +35,24 @@ public class MainMenu : MonoBehaviour
     public void OnQuitButtonClicked()
     {
         Application.Quit();
+    }
+
+    public void Update()
+    {
+        this._avatarChangeCountdown -= Time.deltaTime;
+        if (this._avatarChangeCountdown <= 0f)
+        {
+            this._avatarChangeCountdown = this.AvatarChangeInterval;
+
+            this.ChangeAppearances();
+        }
+    }
+
+    private void ChangeAppearances()
+    {
+        foreach (var avatar in this.Avatars)
+        {
+            avatar.Appearance = this.BodyPartsController.GetRandomAppearance();
+        }
     }
 }
